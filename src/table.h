b70e5f74908bd962af49918241d464518e9c9ceb
Semyon <zhigunovsemen@gmail.com>
2025-06-05 11:49:26 +0300

    Добавил функцию сложения символов строки

--- src/table.h
+++ src/table.h
@@ -1,15 +1,19 @@
 #pragma once
 #include <array>
 #include <cstddef>
+#include <cstdint>
 #include <optional>
 #include <string>
 #include <string_view>
 
 template <std::size_t N = 384> class Table {
+private:
 	using Container = std::array<std::optional<std::string>, N>;
 	Container data_;
 
 	void push_(std::string_view key, bool replace);
+
+	static int64_t syms_sum(std::string_view str) const noexcept;
 
 public:
 	auto get(std::string_view key) const -> std::optional<std::string> const &;
@@ -34,4 +38,12 @@ auto Table<N>::get([[maybe_unused]] std::string_view key) const -> std::optional
 template <std::size_t N> auto Table<N>::get([[maybe_unused]] std::string_view key) -> std::optional<std::string> &
 {
 	return data_[0];
+}
+
+template <std::size_t N> static int64_t Table<N>::syms_sum(std::string_view str) const noexcept
+{
+	int64_t sum{};
+	for (char c : str)
+		sum += (unsigned)c;
+	return sum;
 }
