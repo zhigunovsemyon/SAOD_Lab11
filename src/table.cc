d61ae1901886cfe8165dbbdc8dcf0dee90b126e2
Semyon <zhigunovsemen@gmail.com>
2025-06-05 12:47:32 +0300

    Вынес отдельно наивную хф

--- src/table.cc
+++ src/table.cc
@@ -1,4 +1,6 @@
 #include "table.h"
+#include <cmath>
+#include <cstddef>
 #include <cstdint>
 #include <stdexcept>
 
@@ -10,13 +12,18 @@ static int64_t syms_sum_(std::string_view str) noexcept
 	return sum;
 }
 
+static size_t hash_function_(std::size_t key, [[maybe_unused]] double A, std::size_t m)
+{
+	return key % m;
+}
+
 void Table::push(std::string_view key)
 {
 	auto syms_sum = syms_sum_(key);
 	if (syms_sum < 0)
 		throw std::length_error{"Слишком большая строка!"};
 
-	data_[(size_t)syms_sum % N] = key;
+	data_[hash_function_((size_t)syms_sum, 0, N)] = key;
 }
 
 auto Table::get(std::string_view key) const -> std::optional<std::string_view>
@@ -25,5 +32,6 @@ auto Table::get(std::string_view key) const -> std::optional<std::string_view>
 	if (syms_sum < 0)
 		return std::nullopt;
 
-	return data_[(size_t)syms_sum % N];
+	// return data_[(size_t)syms_sum % N];
+	return data_[hash_function_((size_t)syms_sum, 0, N)];
 }
