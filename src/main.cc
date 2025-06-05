339277a8d711ad9a3634cee808a2a9a99f6b5b37
Semyon <zhigunovsemen@gmail.com>
2025-06-05 11:20:45 +0300

    Заложил протипы геттеров и пушеров

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,10 @@
 #include <print>
+#include "table.h"
+
 int main()
 {
+	Table t;
+	std::println("{}",t.capacity());
+	std::println("{}",t.get("").value_or("Empty"));
 	std::println("Hello World!");
 }
---
+++ src/table.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <array>
+#include <cstddef>
+#include <optional>
+#include <string>
+#include <string_view>
+
+template <std::size_t N = 384> class Table {
+	using Container = std::array<std::optional<std::string>, N>;
+	Container data_;
+
+	void push_(std::string_view key, bool replace);
+
+public:
+	auto get(std::string_view key) const -> std::optional<std::string> const &;
+
+	auto get(std::string_view key) -> std::optional<std::string> &;
+
+	void push_or_replace(std::string_view key) { push_(key, true); }
+
+	void push_no_replace(std::string_view key) { push_(key, false); }
+
+	auto capacity() const noexcept { return data_.size(); }
+};
+
+template <std::size_t N> void Table<N>::push_([[maybe_unused]] std::string_view key, [[maybe_unused]] bool replace) {}
+
+template <std::size_t N>
+auto Table<N>::get([[maybe_unused]] std::string_view key) const -> std::optional<std::string> const &
+{
+	return data_[0];
+}
+
+template <std::size_t N> auto Table<N>::get([[maybe_unused]] std::string_view key) -> std::optional<std::string> &
+{
+	return data_[0];
+}
